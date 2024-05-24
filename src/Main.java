a0b54b12119daee7e06aed468f12c2cbe2caa42a
NotMatteo123 <matteodevecchi16@gmail.com>
2024-05-23 21:31:03 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calo was here");
+        System.out.println("Matteo was here");
 
         // make a frame with size
         JFrame frame = new JFrame("Swing Paint");
