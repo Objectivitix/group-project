532cb63b5c264365f67b491e278219990fbec9a3
FionaB1 <147102365+FionaB1@users.noreply.github.com>
2024-05-27 11:02:09 -0400

    fixed submit button and making a new jFrame now works, dropdown still not functional

--- src/SettingScreen.java
+++ src/SettingScreen.java
@@ -5,13 +5,21 @@ import java.util.InputMismatchException;
 
 public class SettingScreen extends JFrame{
     //Create variables
+    //save file??
+    //upload image??
     public int height;
     private int length;
+    private boolean hasHeight;
+    private boolean hasLength;
     private JFrame settingFrame = new JFrame();
     private JPanel topPanel = new JPanel();
     private JPanel middlePanel = new JPanel();
     private JPanel bottomPanel = new JPanel();
     public SettingScreen() {
+        //set submit conditions to false
+        hasLength = false;
+        hasHeight = false;
+
         //Make frame
         settingFrame.setSize(400, 300);//Setting the size of the frame
         settingFrame.setLayout(null);//Setting the layout of the frame to null
@@ -25,67 +33,104 @@ public class SettingScreen extends JFrame{
         bottomPanel.setBounds(0,200,400, 100);
         settingFrame.add(bottomPanel);
 
-        //Make top label and textField
-        JLabel askHeight = new JLabel("Enter canvas height in pixels:");//label to ask for user's input for name
+        //top label - textFields
+        JLabel askHeight = new JLabel("Enter canvas height in pixels:");
         topPanel.add(askHeight);//Add enter label to textPanel
         JTextField enterHeight = new JTextField(10);//Create textbox for entering height
         topPanel.add(enterHeight);//adds name textField to textPanel
 
-        //Make middle label and textField
-        JLabel askLength = new JLabel("Enter canvas length in pixels:");//label to ask for user's input for name
-        middlePanel.add(askLength);//Add enter label to textPanel
-        JTextField enterLength = new JTextField(10);//Create textbox for entering length
-        middlePanel.add(enterLength);//add textField to textPanel
+        JLabel warning = new JLabel("<html>Dimensions should be greater than 300 each, <br> otherwise canvas size is set to 600x600 automatically.</html>");
+        topPanel.add(warning);
 
-        //Make bottom label and textField
-        JLabel askColor = new JLabel("Choose canvas color:");//label to ask for user's input for name
-        bottomPanel.add(askColor);//Add enter label to textPanel
+        JLabel askLength = new JLabel("Enter canvas length in pixels:");
+        topPanel.add(askLength);//Add enter label to textPanel
+        JTextField enterLength = new JTextField(10);//Create textbox for entering length
+        topPanel.add(enterLength);//add textField to textPanel
+
+        // middle label dropdown
+        JLabel askColor = new JLabel("Choose canvas color:");
+        middlePanel.add(askColor);//Add enter label to textPanel
 
         //weird dropdown code?
         String[] options = {"Green", "Blue", "Red", "Orange"};
         JComboBox<String> comboBox = new JComboBox<>(options);
         comboBox.setSelectedIndex(0); // Set the default selected item
 
-        bottomPanel.add(comboBox);//add textField to textPanel
+        middlePanel.add(comboBox);//add textField to textPanel
+
+        //bottom panel submit button & message label
+        JLabel message = new JLabel("");
+        settingFrame.add(message);
+
+        JButton submit = new JButton("New canvas");
+        bottomPanel.add(submit);
 
         settingFrame.setVisible(true);//make frame visible
 
 
         enterHeight.addActionListener(new ActionListener() {//Create new ActionListener to detect textField interaction
-            @Override//override the existing preset ActionEvent method??
+            @Override
             public void actionPerformed(ActionEvent e) {//when word is entered and enter is clicked
-                try {
-                    height = Integer.parseInt(enterHeight.getText());//assign text from textField to petName variable
-                    //enterHeight.setText("");//replace user's input with thanks
-                } catch (InputMismatchException o){
-                    askHeight.setText("Please enter an integer!");
+                try{
+                    height = Integer.parseInt(enterHeight.getText());//assign text from textField to height variable, parse to int
+                    //enterHeight.setText("");//replace user's input with nothing
+                    hasHeight = true;
+                } catch (Exception o){
                     enterHeight.setText("");
+                    message.setText("Please enter an integer!");
                 }
             }
         });
 
         enterLength.addActionListener(new ActionListener() {//Create new ActionListener to detect textField interaction
-            @Override//override the existing preset ActionEvent method??
+            @Override
             public void actionPerformed(ActionEvent e) {//when word is entered and enter is clicked
                 try {
-                    length = Integer.parseInt(enterLength.getText());//assign text from textField to petName variable
-                    //enterHeight.setText("");//replace user's input with thanks
-                } catch (InputMismatchException o){
-                    askLength.setText("Please enter an integer!");
+                    length = Integer.parseInt(enterLength.getText());//assign text from textField to length variable, parse to int
+                    //enterHeight.setText("");//replace user's input with nothing
+                    hasLength = true;
+                } catch (Exception o){
                     enterLength.setText("");
+                    message.setText("Please enter an integer!");
                 }
             }
         });
 
-        enterLength.addActionListener(new ActionListener() {//Create new ActionListener to detect textField interaction
-            @Override//override the existing preset ActionEvent method??
-            public void actionPerformed(ActionEvent e) {//when word is entered and enter is clicked
-                try {
-                    length = Integer.parseInt(enterLength.getText());//assign text from textField to petName variable
-                    //enterHeight.setText("");//replace user's input with thanks
-                } catch (InputMismatchException o){
-                    askLength.setText("Please enter an integer!");
-                    enterLength.setText("");
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    height = Integer.parseInt(enterHeight.getText());//assign text from textField to height variable, parse to int
+                    //enterHeight.setText("");//replace user's input with nothing
+                    hasHeight = true;
+                } catch (Exception o){
+                    enterHeight.setText("");
+                    message.setText("Both numbers must be integers!");
+                }
+                if(hasHeight && hasLength) {
+                    if(height >= 300 && length >=300) {
+                        JFrame frame = new JFrame("Swing Paint");
+                        frame.setSize(length, height);
+                        // make sure when frame is closed, program terminates
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                        // add top-level panel to frame (this helps swap screens later)
+                        frame.add(new AppScreen());
+
+                        // make everything visible
+                        frame.setVisible(true);
+                    } else{
+                        JFrame frame = new JFrame("Swing Paint");
+                        frame.setSize(600, 600);
+                        // make sure when frame is closed, program terminates
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                        // add top-level panel to frame (this helps swap screens later)
+                        frame.add(new AppScreen());
+
+                        // make everything visible
+                        frame.setVisible(true);
+                    }
                 }
             }
         });
