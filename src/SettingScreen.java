79cb6e622e8883d4dfcca64c39f0c12bad0a565a
FionaB1 <147102365+FionaB1@users.noreply.github.com>
2024-05-30 19:38:15 -0400

    Mostly thanks to Calo setting screen is now fully functional! I fixed error messages too.

--- src/SettingScreen.java
+++ src/SettingScreen.java
@@ -20,6 +20,7 @@ public class SettingScreen extends JFrame{
         hasLength = false;
         hasHeight = false;
 
+
         //Make frame
         settingFrame.setSize(400, 300);//Setting the size of the frame
         settingFrame.setLayout(null);//Setting the layout of the frame to null
@@ -28,45 +29,53 @@ public class SettingScreen extends JFrame{
         //Make panels
         topPanel.setBounds(0,0, 400, 100);//sets location and dimensions of textPanel
         settingFrame.add(topPanel);//adds text panel to the Jframe
+
         middlePanel.setBounds(0,100,400, 100);
         settingFrame.add(middlePanel);
+
         bottomPanel.setBounds(0,200,400, 100);
         settingFrame.add(bottomPanel);
+
 
         //top label - textFields
         JLabel warning = new JLabel("<html>Dimensions should be greater than 300 each, <br> otherwise canvas size is set to 600x600 automatically.</html>");
         topPanel.add(warning);
+
         JLabel askHeight = new JLabel("Enter canvas height in pixels:");
         topPanel.add(askHeight);//Add enter label to textPanel
+
         JTextField enterHeight = new JTextField(10);//Create textbox for entering height
         topPanel.add(enterHeight);//adds name textField to textPanel
 
         JLabel askLength = new JLabel("Enter canvas length in pixels:");
         topPanel.add(askLength);//Add enter label to textPanel
+
         JTextField enterLength = new JTextField(10);//Create textbox for entering length
         topPanel.add(enterLength);//add textField to textPanel
+
 
         // middle label dropdown
         /*JLabel askColor = new JLabel("Choose canvas color:");
         middlePanel.add(askColor);//Add enter label to textPanel*/
+
         JButton pickColor = new JButton("Choose background color");
         middlePanel.add(pickColor);
 
-        /*//weird dropdown code?
-        String[] options = {"Green", "Blue", "Red", "Orange"};
-        JComboBox<String> comboBox = new JComboBox<>(options);
-        comboBox.setSelectedIndex(0); // Set the default selected item
-
-        middlePanel.add(comboBox);//add textField to textPanel*/
-
-        //bottom panel submit button & message label
+        //bottom panel (submit button & message label)
         JLabel message = new JLabel("");
-        settingFrame.add(message);
+        bottomPanel.add(message);
 
         JButton submit = new JButton("New canvas");
         bottomPanel.add(submit);
 
         settingFrame.setVisible(true);//make frame visible
+
+        /*weird dropdown code?
+        String[] options = {"Green", "Blue", "Red", "Orange"};
+        JComboBox<String> comboBox = new JComboBox<>(options);
+        comboBox.setSelectedIndex(0); // Set the default selected item
+
+        middlePanel.add(comboBox);//add textField to textPanel*/
 
         pickColor.addActionListener(new ActionListener() {
             @Override
@@ -86,19 +95,17 @@ public class SettingScreen extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 try{
                     height = Integer.parseInt(enterHeight.getText());//assign text from textField to height variable, parse to int
-                    //enterHeight.setText("");//replace user's input with nothing
-                    hasHeight = true;
+                    hasHeight = true;//accept as valid height
                 } catch (Exception o){
-                    //enterHeight.setText("");
+                    System.out.println("Oh no!");
                     message.setText("Height must be an integer!");
                     settingFrame.repaint();
                 }
                 try{
-                    length = Integer.parseInt(enterHeight.getText());//assign text from textField to height variable, parse to int
-                    //enterHeight.setText("");//replace user's input with nothing
-                    hasLength = true;
+                    length = Integer.parseInt(enterLength.getText());//assign text from textField to height variable, parse to int
+                    hasLength = true;//accept as valid length
                 } catch (Exception o){
-                    //enterLength.setText("");
+                    System.out.println("Oh no!");
                     message.setText("Length must be an integer!");
                     settingFrame.repaint();
                 }
@@ -117,6 +124,8 @@ public class SettingScreen extends JFrame{
                         // make everything visible
                         frame.setVisible(true);
 
+                        //close setting screen
+                        settingFrame.dispose();
                     } else{
                         JFrame frame = new JFrame("Swing Paint");
                         frame.setSize(600, 600);
@@ -126,8 +135,13 @@ public class SettingScreen extends JFrame{
                         // add top-level panel to frame (this helps swap screens later)
                         frame.add(new AppScreen(c));
 
+                        frame.pack();
+
                         // make everything visible
                         frame.setVisible(true);
+
+                        //close setting screen
+                        settingFrame.dispose();
                     }
                 }
             }
