79c37b68c0fd37987ac1a88fc85560de7ae05f01
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2024-06-01 12:46:03 -0400

    Huge code cleanup, part two (again)

--- src/SettingScreen.java
+++ src/SettingScreen.java
@@ -1,26 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class SettingScreen extends JFrame{
+public class SettingScreen extends JFrame {
     //Create variables
     public int height;
     private int length;
-    private boolean hasHeight;
-    private boolean hasLength;
-    private Color c = Color.WHITE; // set color to white as default
+
+    // set color to white as default
+    private Color c = Color.WHITE;
 
     //create frame and panels
     private JFrame settingFrame = new JFrame();
     private JPanel topPanel = new JPanel();
     private JPanel middlePanel = new JPanel();
     private JPanel bottomPanel = new JPanel();
-    public SettingScreen() {
-        settingFrame.setTitle("New canvas");
 
-        //Make frame
+    private JButton pickColor;
+
+    public SettingScreen() {
+        // initialize frame
+        settingFrame.setTitle("New Canvas");
         settingFrame.setSize(400, 300);//Setting the size of the frame
         settingFrame.setLayout(null);//Setting the layout of the frame to null
         settingFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Setting the close operation to exit on close(when GUI is closed program stops)
@@ -42,7 +42,7 @@ public class SettingScreen extends JFrame{
         label.setFont(new Font("Brush Script MT", Font.PLAIN, 22));//set font and size
         topPanel.add(label);
 
-        JLabel warning = new JLabel("<html>Dimensions should each be 600 or greater, <br> otherwise canvas size is set to 600x600 automatically.</html>");
+        JLabel warning = new JLabel("<html>Dimensions should each be 300 or greater, <br> otherwise canvas size is set to 700x700 automatically.</html>");
         warning.setFont(new Font("Serif", Font.PLAIN, 14));
         topPanel.add(warning);
 
@@ -60,7 +60,8 @@ public class SettingScreen extends JFrame{
 
 
         //middle panel - color picking button
-        JButton pickColor = new JButton("Choose background color");//make a Jbutton for choosing the background color
+        JButton pickColor = new JButton("Choose background color");
+        pickColor.addActionListener(e -> pickColorPopup());
         middlePanel.add(pickColor);
 
         //bottom panel (submit button & message label)
@@ -70,82 +71,69 @@ public class SettingScreen extends JFrame{
         JButton submit = new JButton("New canvas");
         bottomPanel.add(submit);
 
+        submit.addActionListener(e -> {
+            String userHeight = enterHeight.getText();
+            String userLength = enterLength.getText();
+
+            // if either input is blank, tell user to enter dimensions
+            if (userHeight.isBlank() || userLength.isBlank()) {
+                message.setText("Enter two integers for canvas dimensions.");
+                return;
+            }
+
+            try {
+                height = Integer.parseInt(userHeight); // try to parse height to int
+            } catch (Exception o) {
+                message.setText("Height must be an integer!");
+                settingFrame.repaint();
+                return;
+            }
+
+            try {
+                length = Integer.parseInt(userLength); // try to parse length to int
+            } catch (Exception o) {
+                message.setText("Length must be an integer!");
+                settingFrame.repaint();
+                return;
+            }
+
+            if (height < 300 || length < 300) {
+                height = 700;
+                length = 700;
+            }
+
+            JFrame frame = new JFrame("Swing Paint");
+
+            // make sure when frame is closed, program terminates
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // add screen and resize frame using pack
+            frame.add(new AppScreen(c, height, length));
+            frame.pack();
+
+            // make everything visible
+            frame.setVisible(true);
+
+            //close setting screen
+            settingFrame.dispose();
+        });
+
         settingFrame.setVisible(true);//make frame visible
-
-        pickColor.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                c = JColorChooser.showDialog(null, "Select a color", Color.BLACK);
-                pickColor.setBackground(c);// button text becomes white when BG is too dark
-                pickColor.setForeground(
-                        Utils.calcLuminance(c) < 0.5
-                                ? Color.WHITE : Color.BLACK);
-            }
-        });
-
-        submit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                hasHeight = false;
-                hasLength = false;
-                try {
-                    height = Integer.parseInt(enterHeight.getText());//assign text from textField to height variable, parse to int
-                    hasHeight = true;//accept as valid height
-                } catch (Exception o) {
-                    message.setText("Height must be an integer!");
-                    settingFrame.repaint();
-                }
-                try {
-                    length = Integer.parseInt(enterLength.getText());//assign text from textField to height variable, parse to int
-                    hasLength = true;//accept as valid length
-                } catch (Exception o) {
-                    message.setText("Length must be an integer!");
-                    settingFrame.repaint();
-                }
-                if (hasHeight && hasLength) {
-                    if (height >= 600 && length >= 600) {
-                        JFrame frame = new JFrame("Swing Paint");
-                        frame.setSize(length, height);
-                        // make sure when frame is closed, program terminates
-                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-                        // add top-level panel to frame (this helps swap screens later)
-                        frame.add(new AppScreen(c, height, length));
-
-                        frame.pack();
-
-                        // make everything visible
-                        frame.setVisible(true);
-
-                        //close setting screen
-                        settingFrame.dispose();
-                    } else {
-                        JFrame frame = new JFrame("Swing Paint");
-                        frame.setSize(600, 600);
-                        // make sure when frame is closed, program terminates
-                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-                        // add top-level panel to frame (this helps swap screens later)
-                        frame.add(new AppScreen(c, 600, 600));
-
-                        frame.pack();
-
-                        // make everything visible
-                        frame.setVisible(true);
-
-                        //close setting screen
-                        settingFrame.dispose();
-                    }
-                } else if (!hasHeight && !hasLength){//if neither has been given, output different error message
-                    message.setText("Enter two integers for canvas dimensions.");
-                }
-            }
-        });
     }
-    public int getLength(){
-        return length;//Just in case someone wants to access it from this class?
-    }
-    public int getHeight(){
-        return height;
+
+    private void pickColorPopup() {
+        c = Utils.colorInput("Select canvas background", Color.BLACK);
+
+        // if user cancels input, simply exit method
+        if (c == null) {
+            return;
+        }
+
+        pickColor.setBackground(c);
+
+        // button text becomes white when BG is too dark
+        pickColor.setForeground(
+            Utils.calcLuminance(c) < 0.5
+                ? Color.WHITE : Color.BLACK);
     }
 }
